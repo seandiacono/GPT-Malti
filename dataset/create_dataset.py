ae4739659361c53f11c0d20791c9fa12737f3ecc
Sean Diacono <seandiacono@gmail.com>
2023-01-18 18:47:30 +0100

    :sparkles: Inital Commit

---
+++ dataset/create_dataset.py
@@ -0,0 +1,14 @@
+import datasets
+
+domains = ["blogs", "comics", "court", "eu_docs", "gov_docs", "government_gazzette", "law_eu", "law_mt", "legal", "nonfiction", "parliament", "press_eu", "press_mt", "speeches", "theses", "umlib_oar", "web_general", "wiki"]
+
+entire_string = ""
+
+for domain in domains:
+    dataset = datasets.load_dataset("MLRS/korpus_malti", domain)
+
+    for document in dataset['train']:
+        entire_string += document['text'].join(" ") + " "
+
+    with open("dataset/dataset.txt", "w") as f:
+        f.write(entire_string)
